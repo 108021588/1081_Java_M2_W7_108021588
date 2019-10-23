416b2dc16c5d7bb768d28b8499115e4616363b60
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:36:57 +0800

    被３整除

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn =new Scanner(System.in);
-        float v1 = scn.nextFloat();
-        float maxVal = v1;
-        float minVal = v1;
-    for (int i=1;i<10;i++){
-        v1= scn.nextFloat();
-        if (maxVal<v1){
-            maxVal=v1;
-        }
-        else if (minVal>v1){
-            minVal =v1;
-        }
+        float a = scn.nextFloat();
+    int sum=0;
+    for (int i=3;i<=a;i+=3){
+        sum = sum+i;
     }
-        System.out.printf("Max=%.2f\n",maxVal);
-        System.out.printf("Min=%.2f\n",minVal);
+        System.out.println(sum);
     }
 }
