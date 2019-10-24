48a1861db21a13e85d62a2710ae4ff39ac052a2c
108021588 <108021588@live.asia.edu.tw>
2019-10-24 20:10:04 +0800

    M2Q25 數字總和-有註解版

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,45 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int ans = 0;
         Scanner scn = new Scanner( System.in );
-        int x = scn.nextInt(),ans = 0;;
-        for (int z = 1;z <= x;z++){
+        int x = scn.nextInt();
+        for (int z = 1;z <= x;z++){//將所有數字拿去測試
             boolean ck235 = false;
             for(int i=2; i<z; i++){
-                if(z%i==0){ boolean check = true;
-                    for(int j=2; check && j<i; j++){ if(i%j==0){
-                        check = false; } }
-                    if(check){ if (i==2 || i==3 || i==5){ ck235 = true; }else { ck235 = false; } } } }
-            if (ck235){ ans = ans + z; } }
-        if (x>2){ ans = ans+2; }
-        if (x>3){ ans = ans+3; }
-        if (x>5){ ans = ans+5; }
-        System.out.println(ans); }}
+                if(z%i==0){//可以整除->i是z的因數
+                    boolean check = true;
+                    for(int j=2; check && j<i; j++){
+                        if(i%j==0){//i可以被整除->i不是質因數
+                            check = false;
+                        }
+                    }
+                    if(check){
+
+                        if (i==2 || i==3 || i==5){//判斷質因數是否為2或3或5
+                            ck235 = true;
+                        }else {
+                            ck235 = false;
+
+                        }
+                    }
+                }
+            }
+
+            if (ck235){
+                ans = ans + z;
+            }
+        }
+        if (x>2){//剛剛質數那邊拿走了2、3、5，要補回來
+            ans = ans+2;
+        }
+        if (x>3){
+            ans = ans+3;
+        }
+        if (x>5){
+            ans = ans+5;
+        }
+        System.out.println(ans);
+    }
+}
